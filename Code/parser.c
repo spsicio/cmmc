2e0a838dc5906e5a88f1a54bd75020ff3d4f6c55
spsicio <spsicio@foxmail.com>
2023-09-26 15:57:05 +0800

    lab1: draft framework almost done.

--- Code/parser.c
+++ Code/parser.c
@@ -8,8 +8,112 @@ void read_token() {
   last_token = get_token();
 }
 
+int get_token_precedence() {
+  switch (last_token) {
+    case kASSIGNOP: return 1;
+    case kRELOP: return 4;
+    case kPLUS: return 5;
+    case kMINUS: return 5;
+    case kSTAR: return 6;
+    case kDIV: return 6;
+    case kAND: return 3;
+    case kOR: return 2;
+    case kDOT: return 7;
+    case kLB: return 7;
+    default: return -1;
+  }
+}
 
-int parser_exp() {  // TODO
+int parser_paren_exp() {
+  read_token();
+  if (!parser_exp())
+    return 1;
+  if (last_token != kRP)
+    return 1;
+  return 0;
+}
+
+int parser_val_exp() {
+  read_token();
+  return 0;
+}
+
+int parser_id_exp() {
+  read_token();
+  if (last_token == kLP) {
+    read_token();
+    if (last_token == kLP) {
+      read_token();
+      return 1;
+    }
+    if (!parser_args())
+      return 1;
+    if (last_token != kLP)
+      return 1;
+    read_token();
+  }
+  return 0;
+}
+
+int parser_neg_exp() {
+  read_token();
+  if (!parser_primary())
+    return 1;
+  return 0;
+}
+
+int parser_not_exp() {
+  read_token();
+  if (!parser_primary())
+    return 1;
+  return 0;
+}
+
+int parser_op_rhs(int lst_prec, int lhs) {
+  for (;;) {
+    int cur_prec = get_token_precedence();
+    if (cur_prec < lst_prec) return lhs;
+    Token op = last_token;
+    // need to save: RELOP
+    // special case: LB, DOT
+    read_token();
+    if (op == kDOT) {
+      if (last_token != kID) return 1;
+      lhs = 0;
+      continue;
+    }
+    if (op == kLB) {
+      if (!parser_exp()) return 1;
+      if (last_token != kRB) return 1;
+      read_token();
+      lhs = 0;
+      continue;
+    }
+    int rhs = parser_primary();
+    if (rhs) return 1;
+    int nxt_prec = get_token_precedence();
+    if (cur_prec < nxt_prec) {
+      rhs = parser_op_rhs(cur_prec, rhs);
+    }
+    lhs = 0;  // merge lhs, op, rhs
+  }
+}
+
+int parser_primary() {
+  switch (last_token) {
+    case kINT: case kFLOAT: return parser_val_exp();
+    case kID: return parser_id_exp(); 
+    case kLP: return parser_paren_exp(); 
+    case kMINUS: return parser_neg_exp(); 
+    case kNOT: return parser_not_exp();
+    default: return 1;
+  }
+}
+
+int parser_exp() {
+  if (!parser_primary())
+    return 1;
+  return parser_op_rhs(0, 0);
 }
 
 int parser_vardec() {
@@ -137,7 +241,7 @@ int parser_compst() {
 }
 
 int parser_stmt() {
-  switch(last_token) {
+  switch (last_token) {
     case kLC: {
       if (!parser_compst())
         return 1;
@@ -258,7 +362,7 @@ int parser_extdeflist() {
 }
 
 int parser_stmtlist() {
-  while (last_token != kRC) {
+  if (last_token != kRC) {
     if (!parser_stmt())
       return 1;
     if (!parser_stmtlist())
@@ -267,7 +371,10 @@ int parser_stmtlist() {
   return 0;
 }
 
-int main() {
-
+int parser_program() {
+  read_token();
+  if (!parser_extdeflist())
+    return 1;
+  return 0;
 }
 
--- Code/parser.h
+++ Code/parser.h
@@ -1,6 +1,14 @@
 #ifndef CMMC_PARSER_H
 #define CMMC_PARSER_H
 
+int parser_primary();
+int parser_op_rhs(int, int);
+int parser_paren_exp();
+int parser_val_exp();
+int parser_id_exp();
+int parser_neg_exp();
+int parser_not_exp();
+
 int parser_exp();         // 表達式
 int parser_vardec();      // 變量名
 int parser_dec();         // 可賦值的變量名
@@ -18,6 +26,7 @@ int parser_deflist();
 int parser_extdeclist();
 int parser_extdeflist();
 int parser_stmtlist();
+int parser_program();
 
 #endif  // CMMC_PARSER_H
 
